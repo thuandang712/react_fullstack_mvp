6fd5cad2bc042d44f4e52afe9ff52e8787c5a66e
Thuan Dang <thuandang@Thuans-Mac-mini.local>
2021-08-28 00:49:09 -0500

    add some css

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -1,17 +1,41 @@
 import React from 'react'
+import { Image, Button, Card, Icon, Label } from 'semantic-ui-react'
 
-const PostItem = ({post, getSinglePostItem}) => {
+const PostItem = ({post: {user_id, user_name, post_content, like_count}, getSinglePostItem}) => {
 
     const handleGetPost = (e) => {
         getSinglePostItem(e.target.id)
     }
 
     return (
-        <div className='post-item' >
-            <h2 className='user-name' id={post.user_id} onClick={handleGetPost}>{post.user_name}</h2>
-            <p className='post-content'>{post.post_content}</p>
-            <button className='like-button'>{post.like_count}</button>
-        </div>
+        // <div className='post-item' >
+        //     <h2 className='user-name' id={post.user_id} onClick={handleGetPost}>{post.user_name}</h2>
+        //     <p className='post-content'>{post.post_content}</p>
+        //     <button className='like-button'>{post.like_count}</button>
+        // </div>
+
+        <Card>
+            <Card.Content>
+                <Image
+                    floated='right'
+                    size='mini'
+                    src='https://react.semantic-ui.com/images/avatar/large/steve.jpg'
+                />
+                <Card.Header>{user_name}</Card.Header>
+                <Card.Description>{post_content}</Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+                <Button as='div' labelPosition='right'>
+                    <Button color='teal' basic>
+                        <Icon name='heart' />
+                        Like
+                    </Button>
+                <Label basic color='teal' pointing='left'>
+                    {like_count}
+                </Label>
+                </Button>
+            </Card.Content>
+        </Card>
     )
 }
 
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import PostItem from './PostItem'
+import {Grid} from 'semantic-ui-react'
 
 const Posts = ({posts, getSinglePostItem}) => {
     return (
-        <div className='post-container'>
-            {posts.map( post => (
-                <PostItem key={post.user_id} post={post} getSinglePostItem={getSinglePostItem}/>
-            ))}
-        </div>
+        // <div className='post-container'>
+        //     {posts.map( post => (
+        //         <PostItem key={post.user_id} post={post} getSinglePostItem={getSinglePostItem}/>
+        //     ))}
+        // </div>
+
+        <Grid columns={3} className='post-container'>
+            <Grid.Row>
+                {posts.map( post => (
+                    <Grid.Column key={post.user_id}>
+                        <PostItem post={post} getSinglePostItem={getSinglePostItem}/>
+                    </Grid.Column>
+                ))}
+            </Grid.Row>
+        </Grid>
     )
 }
 
